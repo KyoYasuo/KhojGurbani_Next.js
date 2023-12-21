d09636ea129b8657d59e9f1301e847caab11f6f8
TcaT <kyoyasuo221@gmail.com>
2023-12-21 23:24:20 +0900

    update

--- app/(Pages)/Media/Ragis/page.tsx
+++ app/(Pages)/Media/Ragis/page.tsx
@@ -1,3 +1,4 @@
+import Groups from "@/components/Groups";
 import MediaTable from "@/components/MedaiTable";
 import { getAllRagis, getRagiMedias } from "@/lib/data";
 import Link from "next/link";
@@ -5,8 +6,7 @@ import Link from "next/link";
 export default async function RagisPage() {
 
     const allRagis = await getAllRagis();
-    const groups = Object.keys(allRagis);
-    console.log(groups);
+    
 
     
 
@@ -16,34 +16,7 @@ export default async function RagisPage() {
                 <h1 className="mx-auto max-w-6xl px-4 py-[27px] sm:py-16 md:py-20 lg:py-[100px] text-2xl sm:text-3xl md:text-4xl lg:text-[40px] font-bold text-white">Ragi Directory</h1>
             </div>
             <div className="max-w-6xl px-4 mx-auto">
-                
-                {groups.map((group: string) => (
-                    <div key={group} className="flex flex-col mb-8 shadow-common">
-                        <div className="relative h-8 mb-2">
-                            <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
-                                width="28px" height="28px" viewBox="0 0 173.000000 170.000000"
-                                preserveAspectRatio="xMidYMid meet"
-                                className="w-8 h-8 mx-auto"
-                            >
-                                <g transform="translate(0.000000,170.000000) scale(0.100000,-0.100000)"
-                                    fill="#6B6B6B" stroke="none">
-                                    <path
-                                        d="M150 1067 l0 -632 347 -200 c191 -110 352 -200 358 -200 6 0 167 90 358 199 l347 200 0 633 0 633 -705 0 -705 0 0 -633z" />
-                                </g>
-                            </svg>
-                            <span className="absolute w-full top-[2px] text-center text-sm text-white font-normal">{group}</span>
-                        </div>
-                        <div className="grid grid-cols-6 gap-[14px] px-6">
-                            {allRagis[group].map((item: { id: number; name: string; description: string; attachment_name: string; }) => (
-                                <div key={item.id} className="">
-                                    <img className="" src={item.attachment_name} />
-                                    <div className="text-[#424242] text-sm my-3">{item.name}</div>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                ))}
-
+                <Groups allRagis={allRagis} />
             </div>
         </>
     );
--- components/Groups.tsx
+++ components/Groups.tsx
@@ -1,26 +1,69 @@
 "use client"
 
-export default function Groups(props: { groups: any; }) {
+import { createRef, useRef } from "react";
 
-    const scrollToPosition = () => {
-        // Assuming you want to scroll 1000 pixels down from the top
-        window.scrollTo({
-            top: 1000,
-            behavior: 'smooth', // for smooth scrolling
-        });
-    };
+export default function Groups(props: { allRagis: any; }) {
+
+    const allRagis = props.allRagis;
+    const groups = Object.keys(allRagis);
+
+    // Initialize divRefs as an object with a ref for each group
+    // let divRefs = groups.reduce((acc, group) => {
+    //     acc[group] = createRef();
+    //     return acc;
+    // }, {});
+
+    // const scrollToDiv = (group) => {
+    //     // Access the ref directly from divRefs object
+    //     const selectedRef = divRefs[group];
+    //     if (selectedRef && selectedRef.current) {
+    //         selectedRef.current.scrollIntoView({
+    //             behavior: 'smooth',
+    //             block: 'start'
+    //         });
+    //     }
+    // };
+
 
     return (
-        <div className="flex justify-center my-4">
-            {props.groups.map((group: string) => (
-                <div
-                    key={group}
-                    className="text-[#646464] text-base font-bold rounded-full w-[30px] h-[30px] bg-[#DCDCDC] m-[5px] flex items-center justify-center"
-                    onClick={scrollToPosition}
-                >
-                    <p>{group}</p>
+        <>
+            <div className="flex justify-center my-4">
+                {groups.map((group: string) => (
+                    <div
+                        key={group}
+                        className="text-[#646464] text-base font-bold rounded-full w-[30px] h-[30px] bg-[#DCDCDC] m-[5px] flex items-center justify-center"
+                        onClick={() => scrollToDiv(group)}
+                    >
+                        <p>{group}</p>
+                    </div>
+                ))}
+            </div>
+            {groups.map((group: string) => (
+                <div key={group} className="flex flex-col mb-8 shadow-common">
+                    <div className="relative h-8 mb-2">
+                        <svg version="1.0" xmlns="http://www.w3.org/2000/svg"
+                            width="28px" height="28px" viewBox="0 0 173.000000 170.000000"
+                            preserveAspectRatio="xMidYMid meet"
+                            className="w-8 h-8 mx-auto"
+                        >
+                            <g transform="translate(0.000000,170.000000) scale(0.100000,-0.100000)"
+                                fill="#6B6B6B" stroke="none">
+                                <path
+                                    d="M150 1067 l0 -632 347 -200 c191 -110 352 -200 358 -200 6 0 167 90 358 199 l347 200 0 633 0 633 -705 0 -705 0 0 -633z" />
+                            </g>
+                        </svg>
+                        <span className="absolute w-full top-[2px] text-center text-sm text-white font-normal">{group}</span>
+                    </div>
+                    <div className="grid grid-cols-6 gap-[14px] px-6">
+                        {allRagis[group].map((item: { id: number; name: string; description: string; attachment_name: string; }) => (
+                            <div key={item.id} className="">
+                                <img className="" src={item.attachment_name} />
+                                <div className="text-[#424242] text-sm my-3">{item.name}</div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             ))}
-        </div>
+        </>
     );
 }
